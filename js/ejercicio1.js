f11f31c832ab30536873f4c6cb6575d60ec7f713
“kppr0524” <kppr.0524@gmail.com>
2023-08-14 21:10:38 -0500

    Subiendo proyecto

---
+++ js/ejercicio1.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ * Ejercicio 1: Saludo simple
+ * Crea un programa que utilice alert() para mostrar el mensaje: "¡Hola! ¡Bienvenido/a  a JavaScript!"
+ * 
+ * */
+
+let Saludo = "¡Hola! ¡Bienvenido/a  a JavaScript!"
+
+alert (Saludo);
---
+++ js/ejercicio2.js
@@ -0,0 +1,10 @@
+/*
+* Ejercicio 2: Conversión a minúsculas
+* Solicita al usuario que ingrese una palabra o frase utilizando prompt(). 
+* Luego, muestra la entrada del usuario en minúsculas utilizando toLowerCase().
+*/
+
+let userInput = prompt("Ingrese una palabra o frase:");
+let userInputLowerCase = userInput.toLowerCase();
+
+alert("Entrada del usuario en minúsculas: " + userInputLowerCase);
